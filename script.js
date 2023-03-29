8bf28405a94618a1ec2e64d0b8cf405c8de393bd
Edward Wynman <edward.wynman@gmail.com>
2023-03-29 01:08:56 -0400

    Automated commit

---
+++ script.js
@@ -0,0 +1,62 @@
+(function ($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          1000,
+          "easeInOutExpo"
+        );
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $(".js-scroll-trigger").click(function () {
+    $("menu").collapse("hide");
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $("body").scrollspy({
+    target: "#sideNav",
+  });
+})(jQuery); // End of use strict
+
+/*==================================COUNTDOWN================================ */
+// var countDownDate = new Date("Feb 23, 2023 21:30:00").getTime();
+
+// var x = setInterval(function () {
+//   var now = new Date().getTime();
+//   var distance = countDownDate - now;
+
+//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//   var hours = Math.floor(
+//     (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+//   );
+//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//   document.getElementById("countdown").innerHTML =
+//     days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+//   if (distance < 0) {
+//     clearInterval(x);
+//     document.getElementById("countdown").innerHTML = "EXPIRED";
+//   }
+// }, 1000);
+
+/*==================================CLEAR VENT BOX================================ */
+function clearText() {
+  document.querySelector(".vent-box").value = "";
+}
